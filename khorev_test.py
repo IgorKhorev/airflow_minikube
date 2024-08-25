dc4950c461e574f80cb48eafb3cc144f2494a41d
IgorKhorev <30577807+IgorKhorev@users.noreply.github.com>
2024-08-25 14:34:48 +0500

    Update khorev_test.py

--- khorev_test.py
+++ khorev_test.py
@@ -52,8 +52,8 @@ dag = DAG(
     'khorev_test',
     default_args=default_args,
     description='A simple DAG',
-    #schedule_interval=timedelta(days=1),
-    shedule_interval = '5 7 * * *',   
+    schedule_interval=timedelta(days=1),
+    #shedule_interval = '5 7 * * *',   
 )
 #
 def print_hello():
